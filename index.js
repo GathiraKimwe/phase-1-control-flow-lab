4f53f8f8efe643756d73e7bfba92f71aae11ecf1
Edwin Gathira <gathirakimwe@gmail.com>
2022-08-17 00:19:53 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let newRide = parseInt(ride, 10);
+  if (newRide <= 400) {
+    return "This one is on me!";
+  } else if (newRide >= 2500) {
+    return "No can do.";
+  } else if (newRide >= 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "It's not working";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much."
+  } else if (tip === "not as generous"){
+    return "Thank you."
+  } else {
+    return "Bye.";
+  }
 }
